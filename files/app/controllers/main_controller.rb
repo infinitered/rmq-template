230ae8fc5dde56eb5107cbb757409f39f47756bd
Todd Werth <todd@toddwerth.com>
2013-10-06 18:50:39 -0700

    Added missing folders, read me, and updated main_controller

--- files/app/controllers/main_controller.rb
+++ files/app/controllers/main_controller.rb
@@ -7,31 +7,33 @@ class MainController < UIViewController
     rmq(self.view).apply_style :root_view
 
     # Create your UIViews here
-    rmq.append(UILabel, :hello_world).get
+    @hello_world_label = rmq.append(UILabel, :hello_world).get
   end
 
-  # Use if you are supporting multiple orientations
-  # def supportedInterfaceOrientations
-  #   UIInterfaceOrientationMaskAll
-  # end
+  # Remove if you are only supporting portrait
+  def supportedInterfaceOrientations
+    UIInterfaceOrientationMaskAll
+  end
 
-  # Use if you are supporting multiple orientations
-  # def willAnimateRotationToInterfaceOrientation(orientation, duration: duration)
-  #   rmq.all.reapply_styles
-  # end
-  #
-  # You don't have to reapply styles to all UIViews, if you want to optimize, 
-  # another way to do it is tag the views you need to restyle in your stylesheet, 
-  # then only reapply the tagged views, like so:
-  #
-  # def logo(st)
-  #    st.frame = {t: 10, w: 200, h: 96}
-  #    st.centered = :horizontal
-  #    st.image = image.resource('logo')
-  #    st.tag(:reapply_style)
-  #  end
-  #
-  # Then here in willAnimateRotationToInterfaceOrientation
-  #  rmq(:reapply_style).reapply_styles
+  # Remove if you are only supporting portrait
+  def willAnimateRotationToInterfaceOrientation(orientation, duration: duration)
+    rmq.all.reapply_styles
+  end
 
 end
+
+
+__END__
+
+# You don't have to reapply styles to all UIViews, if you want to optimize, 
+# another way to do it is tag the views you need to restyle in your stylesheet, 
+# then only reapply the tagged views, like so:
+def logo(st)
+  st.frame = {t: 10, w: 200, h: 96}
+  st.centered = :horizontal
+  st.image = image.resource('logo')
+  st.tag(:reapply_style)
+end
+
+# Then in willAnimateRotationToInterfaceOrientation
+rmq(:reapply_style).reapply_styles
